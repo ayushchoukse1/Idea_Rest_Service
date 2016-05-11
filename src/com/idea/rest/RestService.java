c2167cb7a9336b72ef4304bb911231161c405a14
ayush choukse <ayush.choukse@sjsu.edu>
2016-05-10 22:15:07 -0700

    Temperature Action RESTCalls added.

--- src/com/idea/rest/RestService.java
+++ src/com/idea/rest/RestService.java
@@ -108,5 +108,54 @@ public class RestService {
 	}
 
 	
+	/*
+	 * { "_id" : ObjectId("5732bcdadc055c1243707e77"), "deviceID" :
+	 * "28:26:1c:60:07:00:00:ad", "action" : "Water heater is switched off",
+	 * "timestamp" : ISODate("2016-05-11T05:02:40.181Z") }
+	 */
+	
+	
+	@GET
+	@Path("temperature/action/{deviceID}")
+	@Produces("application/json")
+	public String getTemperatureActionByDeviceID(@PathParam("deviceID") String deviceID) {
+		DBCollection collection = MongoDBConnection.getDBConnection().getCollection("tempActions");
+		DBCursor cursor = null;
+		BasicDBObject query = new BasicDBObject();
+		BasicDBObject retreivalObj = null;
+		query.put("deviceID", deviceID);
+		cursor = collection.find(query);
+		String err = " { \"Error\": \"Action Not Found\" }";
+		if (cursor.count() == 0) {
+			return err;
+		} else {
+			retreivalObj = (BasicDBObject) cursor.next();
+		}
+		return retreivalObj.toString();
+	}
+	
+	@GET
+	@Path("temperature/action/{deviceID}/{key}")
+	@Produces("application/json")
+	public String  getTemperatureActionByDeviceIDAndKey(@PathParam("deviceID") String deviceID,@PathParam("key") String key) {
+		DBCollection collection = MongoDBConnection.getDBConnection().getCollection("tempActions");
+		DBCursor cursor = null;
+		BasicDBObject query = new BasicDBObject();
+		BasicDBObject retreivalObj = null;
+		query.put("deviceID", deviceID);
+		cursor = collection.find(query);
+		String err = " { \"Error\": \"Action Not Found\" }";
+		if (cursor.count() == 0) {
+			return err;
+		} else {
+			retreivalObj = (BasicDBObject) cursor.next();
+		}
+		
+		if (retreivalObj.containsKey(key)) {
+			return retreivalObj.getString(key);
+		} else {
+			return err;
+		}
+	}
 	
 }
